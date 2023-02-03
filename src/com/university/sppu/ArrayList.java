c706a1e0a0ccb02d10a1aea5f65f96cabebe11f8
priyanka <priyankagulbhile@gmail.com>
2023-02-03 14:09:51 +0530

    string reversed

--- src/com/university/sppu/ArrayList.java
+++ src/com/university/sppu/ArrayList.java
@@ -4,17 +4,13 @@ import java.util.HashSet;
 
 public class ArrayList {
 
-	public static void main(String[] args) {
-		
-		HashSet hs = new HashSet();
-		hs.add(5);
-		hs.add(8);
-		hs.add(3);
-		System.out.println(hs);
-		
-		
+	public static void main(String[] args) 
+	{
+		HashSet<Integer> hashset = new HashSet<Integer>();
+		hashset.add(5);
+		hashset.add(8);
+		hashset.add(3);
+		System.out.println(hashset);		
 	}
 
-	
-
-}
+ }
--- src/com/university/sppu/Demo.java
+++ src/com/university/sppu/Demo.java
@@ -3,10 +3,16 @@ package com.university.sppu;
 public class Demo {
 
 	public static void main(String[] args) {
-		
+	
 		String str = "hello all";
-		System.out.println(str);
-
+	 char ch[] = str.toCharArray();
+	 String reverse = " ";
+		System.out.println("original string is::"+str);
+		for(int i=ch.length-1;i>=0;i--)
+		{
+			reverse = reverse + ch[i];
+		}
+		System.out.println("reverse string is::"+reverse);
 	}
 
 }
